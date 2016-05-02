b252493af8592f24fc90804bfc4edcced6a02c89
Thomas CAPET <capettho@inf-48-tst>
2016-05-02 08:29:05 +0200

    Ajout de la methode post pour /depot

--- src/main/java/fr/univ/rouen/clientWeb/controller/ClientWeb.java
+++ src/main/java/fr/univ/rouen/clientWeb/controller/ClientWeb.java
@@ -14,18 +14,22 @@ import fr.univ.rouen.clientWeb.model.StbModelVO;
 
 public class ClientWeb {
 	
-	public String uri;
+	private String uri;
 	
-	public final static ClientWeb instance =  new ClientWeb();
+	private final static ClientWeb instance =  new ClientWeb();
 	
 	private ClientWeb() {
 		uri = "http://rocky-gorge-86159.herokuapp.com/";
+	}
+	
+	public String getUri() {
+		return uri;
 	}
 
 	public static String getFrontPage()
 	{
 	    RestTemplate restTemplate = new RestTemplate();
-	    String result = restTemplate.getForObject(instance.uri + "/", String.class);
+	    String result = restTemplate.getForObject(getInstance().getUri() + "/", String.class);
 	     
 	    return result;
 	}
@@ -36,7 +40,7 @@ public class ClientWeb {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<StbListVO> response = restTemplate.exchange(instance.uri + "/stbs", 
+		ResponseEntity<StbListVO> response = restTemplate.exchange(getInstance().getUri() + "/resume", 
 				HttpMethod.GET, entity, StbListVO.class);
 		StbListVO result = response.getBody();
 	     
@@ -49,25 +53,28 @@ public class ClientWeb {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
 		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<StbModelVO> response = restTemplate.exchange(instance.uri + "/stbs/{id}", 
+		ResponseEntity<StbModelVO> response = restTemplate.exchange(getInstance().getUri() + "/resume/{id}", 
 				HttpMethod.GET, entity, StbModelVO.class, id);
 		StbModelVO result = response.getBody();
 		
 	    return result;
 	}
 	
-	/*private static String postStb(StbModelVO stb) {
+	private static String postStb(StbModelVO stb) {
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
-		//headers.setContentType(MediaType.APPLICATION_XML);
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_XML));
-		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<String> response = restTemplate.exchange(uri + "/depot", 
+		headers.setContentType(MediaType.APPLICATION_XML);
+		HttpEntity<StbModelVO> entity = new HttpEntity<StbModelVO>(stb, headers);
+		ResponseEntity<String> response = restTemplate.exchange(getInstance().getUri() + "/depot",
 				HttpMethod.POST, entity, String.class);
 		String result = response.getBody();
-	 
+		
 	    return result;
-	}*/
+	}
+	
+	public static ClientWeb getInstance() {
+		return instance;
+	}
 	
 	
 	public static void main(String[] args){
